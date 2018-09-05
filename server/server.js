e4ce8331b6d4b2233fe6f8823c29844c45244f92
Michal Havel <michal.havel.pdy@seznam.cz>
2018-09-05 15:31:02 +0200

    md

--- server/server.js
+++ server/server.js
@@ -58,11 +58,11 @@ app.set('port', process.env.PORT || 3030); // main port
 
 // prepare our API endpoint routing
 var oauth = require('./oauth');
-var dm = require('./data.management/');
-var md = require('./model.derivative/');
+var dm = require('./data.management');
+var md = require('./model.derivative');
 app.use('/', oauth); // redirect oauth API calls
-app.use('/dm', dm); // redirect our Data Management API calls
-app.use('/md', md); // redirect our Data Management API calls
+app.use('/', dm); // redirect our Data Management API calls
+app.use('/', md); // redirect our Data Management API calls
 
 
 module.exports = app;
