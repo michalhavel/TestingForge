acc48f6ddafc2577850e779d4d708f93deb07311
Michal Havel <michal.havel.pdy@seznam.cz>
2018-11-08 23:02:21 +0100

    Oprava getAccessToken na getLegToken

--- js/forge.viewer.js
+++ js/forge.viewer.js
@@ -7,7 +7,7 @@ var propValue;
 
 var options = {
     env: 'AutodeskProduction',
-    getAccessToken: get3LegToken
+    getAccessToken: getLegToken
 }
 
 var documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE4LTAyLTI4LTA4LTI1LTE0LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL0QtNjIwLTAwMC1ObyUyMEV4cHJlc3MuZHdmeA';
