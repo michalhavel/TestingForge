c814ed471a047c605b95f67eec641830e99d7dad
Michal Havel <michal.havel.pdy@seznam.cz>
2018-11-16 10:17:07 +0100

    doplnena slozka fontu

--- server/server.js
+++ server/server.js
@@ -53,7 +53,8 @@ app.use('/', express.static(__dirname + '/..')); // redirect static calls
 app.use('/js', express.static(__dirname + '/../node_modules/bootstrap/dist/js')); // redirect static calls
 app.use('/js', express.static(__dirname + '/../node_modules/jquery/dist')); // redirect static calls
 app.use('/css', express.static(__dirname + '/../node_modules/bootstrap/dist/css')); // redirect static calls
-app.use('/fonts', express.static(__dirname + '/../node_modules/bootstrap/dist/fonts')); // redirect static calls
+app.use('/fonts', express.static(__dirname + '/../node_modules/bootstrap/dist/fonts'));
+app.use('/font',express.static(__dirname + '/../font')) // redirect static calls
 app.set('port', process.env.PORT || 3030); // main port
 
 // prepare our API endpoint routing
